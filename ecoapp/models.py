9110974791e937f00551784f9c190d0705584788
unknown <szhokee@gmail.com>
2023-12-13 19:18:02 +0600

    day2k

--- ecoapp/models.py
+++ ecoapp/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField()
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image_url = models.URLField()
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price_per_unit * self.quantity
+
+class Order(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    items = models.ManyToManyField(CartItem)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.name}"
