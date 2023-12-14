36eb6328c79c4b286dd41e2fb9eb51ee6e946e9a
unknown <szhokee@gmail.com>
2023-12-14 18:59:13 +0600

    day3ew

--- ecoapp/urls.py
+++ ecoapp/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from django.urls import path
-from .views import YourMainView
 from .views import CategoryListView, ProductListView, CartView, OrderView
 
 urlpatterns = [
     # Эндпоинты для категорий и товаров
-    path('your-main-view/', YourMainView.as_view(), name='your-main-view'),
     path('api/categories/', CategoryListView.as_view(), name='category-list'),
     path('products/', ProductListView.as_view(), name='product-list'),
     path('api/products/<int:category_id>/', ProductListView.as_view(), name='product-list'),
