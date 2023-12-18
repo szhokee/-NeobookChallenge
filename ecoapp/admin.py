79a381726e134784d9a5d3ff293d5c5808313491
unknown <szhokee@gmail.com>
2023-12-18 18:59:58 +0600

    day7qs

--- ecoapp/admin.py
+++ ecoapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Product, CartItem, Order
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image_url')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_per_unit', 'category')
+    list_filter = ('category',)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'quantity')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'address', 'total_price')
+    readonly_fields = ('total_price',)
+    filter_horizontal = ('items',)
